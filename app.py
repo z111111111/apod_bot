a9490d9c508ab65970abcb020f1bbaea07ea33ad
z1 <ee.lazko@ya.ru>
2021-05-16 17:27:02 +0300

    1.5

--- app.py
+++ app.py
@@ -144,4 +144,4 @@ def webhook():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8443)))
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
